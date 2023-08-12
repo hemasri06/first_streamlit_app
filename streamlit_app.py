09dd283b045cbd704b6158ac86d129700b9b9fe1
hemasri06 <142114748+hemasri06@users.noreply.github.com>
2023-08-13 00:34:54 +0530

    Update streamlit_app.py
    typo fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatemeanl ')
 streamlit.text('🥗 Kale , spinach & Rocket smoothies')
 streamlit.text('🐔 Hard-boiled Free-Range Egg')
-streamlit.test('🥑🍞 Avocodo Toast')
+streamlit.text('🥑🍞 Avocodo Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
