e1ea004e29bfa2ffcefaa0b924552a1c522a9cd5
hemasri06 <142114748+hemasri06@users.noreply.github.com>
2023-08-14 23:14:27 +0530

    Update streamlit_app.py
    wdFA

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
-
+streamlit.text("Hello from Snowflake:")
 import snowflake.connector
 streamlit.text("Hello from Snowflake:")
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
