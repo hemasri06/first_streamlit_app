3c325838db618fe3e1e09431ade2265a269ec4ba
hemasri06 <142114748+hemasri06@users.noreply.github.com>
2023-08-15 15:42:46 +0530

    Update streamlit_app.py
    adding streamlit column

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,8 +37,6 @@ streamlit.dataframe(my_data_rows)
 import requests
 fruit_choice = streamlit.text_input('What fruit would you like to add')
 streamlit.write('Thanks for adding ', fruit_choice)
-# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# streamlit.dataframe(fruityvice_normalized)
 
+my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
 
