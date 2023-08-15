89a1b2d966a54d20226a80d565c8877184c90be8
hemasri06 <142114748+hemasri06@users.noreply.github.com>
2023-08-15 17:21:49 +0530

    Update streamlit_app.py
    organised code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas
-
+import requests
+import snowflake.connector
+from urllib.error import URLerror
 
 streamlit.title('My Moms New Healthy Diner')
 
@@ -19,14 +21,16 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-import requests
+# import requests
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+
+streamlit.stop()
+# import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
@@ -34,7 +38,7 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("Fruit load list contain:")
 streamlit.dataframe(my_data_rows)
 
-import requests
+# import requests
 fruit_choice = streamlit.text_input('What fruit would you like to add')
 streamlit.write('Thanks for adding ', fruit_choice)
 
